ebd40c23260d5f9b39d2188f152d8e3bac5eb506
Pawel Koperek <pkoperek@gmail.com>
2014-04-19 15:09:59 +0100

    renaming variables

--- base_templ_gen.sh
+++ base_templ_gen.sh
@@ -40,22 +40,22 @@ chmod +x ${CLEAN_UP}
 echo "#!/bin/bash" >> ${CLEAN_UP}
 
 echo -n "Uploading template..."
-TEMPLATE_ID=`onetemplate create $OUTTMP_F | grep ID | awk -F':' '{print $2}'|tr -d ' '`
-echo "onetemplate delete ${TEMPLATE_ID}" >> ${CLEAN_UP}
-echo "Done. Template ID: ${TEMPLATE_ID}"
+BASE_TEMPLATE_ID=`onetemplate create $OUTTMP_F | grep ID | awk -F':' '{print $2}'|tr -d ' '`
+echo "onetemplate delete ${BASE_TEMPLATE_ID}" >> ${CLEAN_UP}
+echo "Done. Template ID: ${BASE_TEMPLATE_ID}"
 
 echo "Starting base node for customization..."
-VM_ID=`onetemplate instantiate ${TEMPLATE_ID} | grep ID | awk -F':' '{print $2}'|tr -d ' '`
-echo "onevm shutdown $VM_ID" >> ${CLEAN_UP}
-echo "onevm delete $VM_ID" >> ${CLEAN_UP}
+BASE_VM_ID=`onetemplate instantiate ${BASE_TEMPLATE_ID} | grep ID | awk -F':' '{print $2}'|tr -d ' '`
+echo "onevm shutdown $BASE_VM_ID" >> ${CLEAN_UP}
+echo "onevm delete $BASE_VM_ID" >> ${CLEAN_UP}
 	
-MASTER_IP=`onevm show ${VM_ID} |grep IP|awk -F'"' '{print $2}'`
-waitUntilState $VM_ID "ACTIVE"
-echo "Done. Internal IP: ${MASTER_IP}"
+BASE_IP=`onevm show ${BASE_VM_ID} |grep IP|awk -F'"' '{print $2}'`
+waitUntilState $BASE_VM_ID "ACTIVE"
+echo "Done. Internal IP: ${BASE_IP}"
 
 echo -n "Forwarding ssh..."
 IPPORT_LINE=`expect -c "log_file expect.log
-	spawn oneport -a $MASTER_IP -p 22 
+	spawn oneport -a $BASE_IP -p 22 
 	expect \"Username:  \" 
 	send $USER\n
 	expect \"Password:  \"
@@ -64,29 +64,29 @@ IPPORT_LINE=`expect -c "log_file expect.log
 "`
 echo "Done."
 
-MASTER_IPPORT_OUT=`echo "$IPPORT_LINE"|grep ">"| awk -F"->" '{print $1}'`
-MASTER_IP_OUT=`echo "$MASTER_IPPORT_OUT"|awk -F":" '{print $1}'|tr -d ' '`
-MASTER_IP_OUT=`echo "$MASTER_IP_OUT"|awk '{print $1}'`
-MASTER_PORT_OUT=`echo "$MASTER_IPPORT_OUT"|awk -F":" '{print $2}'|tr -d ' '`
+BASE_IPPORT_OUT=`echo "$IPPORT_LINE"|grep ">"| awk -F"->" '{print $1}'`
+BASE_IP_OUT=`echo "$BASE_IPPORT_OUT"|awk -F":" '{print $1}'|tr -d ' '`
+BASE_IP_OUT=`echo "$BASE_IP_OUT"|awk '{print $1}'`
+BASE_PORT_OUT=`echo "$BASE_IPPORT_OUT"|awk -F":" '{print $2}'|tr -d ' '`
 
-echo "Master access forwarded to: $MASTER_IP_OUT $MASTER_PORT_OUT (\"ssh -p "$MASTER_PORT_OUT" root@"$MASTER_IP_OUT"\")"
+echo "Master access forwarded to: $BASE_IP_OUT $BASE_PORT_OUT (\"ssh -p "$BASE_PORT_OUT" root@"$BASE_IP_OUT"\")"
 echo "Copying installation script ..."
-uploadFile "${MASTER_IP_OUT}" "${MASTER_PORT_OUT}" "mesos_install.sh"
+uploadFile "${BASE_IP_OUT}" "${BASE_PORT_OUT}" "mesos_install.sh"
 echo "Done."
 
 echo -n "Executing installation script..."
-ssh -oStrictHostKeyChecking=no -p "$MASTER_PORT_OUT" root@"${MASTER_IP_OUT}" 'chmod +x mesos_install.sh && ./mesos_install.sh' >& mesos_installation.log
+ssh -oStrictHostKeyChecking=no -p "$BASE_PORT_OUT" root@"${BASE_IP_OUT}" 'chmod +x mesos_install.sh && ./mesos_install.sh' >& mesos_installation.log
 echo "Done."
 
 echo "Storing mesos-ready image..."
 echo -n "Generating image ID..."
-BASE_IMAGE_ID=`onevm saveas $VM_ID $CLUSTER_NAME-base-image|grep "Image ID"|awk -F":" '{print $2}'|tr -d ' '`
+BASE_IMAGE_ID=`onevm saveas $BASE_VM_ID $CLUSTER_NAME-base-image|grep "Image ID"|awk -F":" '{print $2}'|tr -d ' '`
 echo "oneimage delete $BASE_IMAGE_ID" >> $CLEAN_UP
 echo "Done. Base ID: $BASE_IMAGE_ID"
 
 echo -n "Storing the image..."
-onevm shutdown $VM_ID
-waitUntilState $VM_ID "DONE"
+onevm shutdown $BASE_VM_ID
+waitUntilState $BASE_VM_ID "DONE"
 echo "Done."
 
 echo -n "Starting master..."
