1314c1d8c6f4ca53c6a9a3757282b5c64cb7339e
Pawel Koperek <pkoperek@gmail.com>
2014-04-20 17:15:01 +0100

    fixing typo

--- common.sh
+++ common.sh
@@ -107,7 +107,7 @@ function setupVM {
 	echo "Done."
  
 	echo "Access forwarded to: $SETUP_IP_OUT $SETUP_PORT_OUT (\"ssh -p "$SETUP_PORT_OUT" root@"$SETUP_IP_OUT"\")"
-	echo "ssh -p "$SETUP_PORT_OUT" root@"$SETUP_IP_OUT" >> "${CLUSTER_ACCESS}"
+	echo "ssh -p $SETUP_PORT_OUT root@$SETUP_IP_OUT" >> "${CLUSTER_ACCESS}"
 
 	echo "Copying script ($FILE)..."
 	uploadFile "${SETUP_IP_OUT}" "${SETUP_PORT_OUT}" "${FILE}"
