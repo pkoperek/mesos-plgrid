df9499d9da178a3705927905511c2eab7f6829aa
Pawel Koperek <pkoperek@gmail.com>
2014-04-21 15:11:41 +0100

    setup ip in default/mesos-*

--- master_setup.sh
+++ master_setup.sh
@@ -2,4 +2,8 @@
 
 mv /etc/init/mesos-slave.conf /root
 
+MY_IP=`ifconfig eth0|grep "inet addr"|awk -F":" '{print $2}'| awk '{print $1}'`
+echo "IP=$MY_IP" >> /etc/default/mesos-master
+
+
 reboot
--- slave_setup.sh
+++ slave_setup.sh
@@ -1,6 +1,8 @@
 mv /etc/init/mesos-master.conf /root
 mv /etc/init/zookeeper.conf /root
 
+MY_IP=`ifconfig eth0|grep "inet addr"|awk -F":" '{print $2}'| awk '{print $1}'`
+echo "IP=$MY_IP" >> /etc/default/mesos-slave
 echo "zk://${MASTER_IP}:2181/mesos" > /etc/mesos/zk
 
 reboot
