1ed85bffc33fc5ba54412452eea07479b69e0665
Pawel Koperek <pkoperek@gmail.com>
2014-04-21 15:18:53 +0100

    slave hostname

--- slave_setup.sh
+++ slave_setup.sh
@@ -1,8 +1,14 @@
 mv /etc/init/mesos-master.conf /root
 mv /etc/init/zookeeper.conf /root
 
+# ip
 MY_IP=`ifconfig eth0|grep "inet addr"|awk -F":" '{print $2}'| awk '{print $1}'`
 echo "IP=$MY_IP" >> /etc/default/mesos-slave
+
+# hostname
+echo "slave$SLAVE_NO" > /etc/hostname
+
+# master ip
 echo "zk://${MASTER_IP}:2181/mesos" > /etc/mesos/zk
 
 reboot
