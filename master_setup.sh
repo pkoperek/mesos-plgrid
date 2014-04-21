2fa6a5cbbe347bd5ec8f87ef97a12fc2cf4f456e
Pawel Koperek <pkoperek@gmail.com>
2014-04-21 15:14:39 +0100

    hostname in master

--- master_setup.sh
+++ master_setup.sh
@@ -2,8 +2,11 @@
 
 mv /etc/init/mesos-slave.conf /root
 
+# ip
 MY_IP=`ifconfig eth0|grep "inet addr"|awk -F":" '{print $2}'| awk '{print $1}'`
 echo "IP=$MY_IP" >> /etc/default/mesos-master
 
+# hostname
+echo "master" > /etc/hostname
 
 reboot
