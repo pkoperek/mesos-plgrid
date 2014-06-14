da5d758813b9010f0d86160decbcd1233a8755ea
pkoperek <pkoperek@gmail.com>
2014-06-14 21:20:14 +0200

    adding master entry in slaves' hosts file

--- slave_setup.sh
+++ slave_setup.sh
@@ -9,16 +9,17 @@ echo "IP=$MY_IP" >> /etc/default/mesos-slave
 HOSTNAME="slave$SLAVE_NO"
 echo "${HOSTNAME}" > /etc/hostname
 
-# master ip
-echo "zk://${MASTER_IP}:2181/mesos" > /etc/mesos/zk
-
+# /etc/hosts
 sed s/plgubuntu/"${HOSTNAME}"/g /etc/hosts > /etc/hosts.new
 cp /etc/hosts /etc/hosts.old
 mv /etc/hosts.new /etc/hosts
+echo "${MASTER_IP}	master" >> /etc/hosts
+
+# zookeeper
+echo "zk://master:2181/mesos" > /etc/mesos/zk
 
 # startup script overwrite
 echo "#!/bin/sh -e" > /etc/rc.local
 echo "sudo /usr/lib/hadoop/sbin/hadoop-daemon.sh start datanode" >> /etc/rc.local
-
 
 reboot
