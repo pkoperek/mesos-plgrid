962ab4dce1166877688e76d9a41bd8515d86045d
pkoperek <pkoperek@gmail.com>
2014-07-27 19:11:29 +0200

    customization script for nibbler project

--- custom_master.sh
+++ custom_master.sh
@@ -10,4 +10,5 @@ echo "export MESOS_NATIVE_LIBRARY=/usr/local/lib/libmesos.so" >> "${SPARK_VERSIO
 echo "export SPARK_EXECUTOR_URI=http://d3kbcqa49mib13.cloudfront.net/spark-1.0.0-bin-hadoop2.tgz" >> "${SPARK_VERSION}/conf/spark-env.sh"
 echo "-Djava.net.preferIPv4Stack=true" >> "${SPARK_VERSION}/conf/java-opts"
 
+
 git clone https://github.com/pkoperek/nibbler.git
