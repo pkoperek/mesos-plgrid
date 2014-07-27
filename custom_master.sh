41e9d04130727b9238260453891041adc5283ca6
Pawel Koperek <pkoperek@gmail.com>
2014-07-27 18:34:12 +0100

    fixing customization script

--- custom_master.sh
+++ custom_master.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-wget http://d3kbcqa49mib13.cloudfront.net/spark-1.0.0-bin-hadoop2.tgz
-tar zxvf spark-1.0.0-bin-hadoop2.tgz
+SPARK_VERSION=spark-1.0.0-bin-hadoop2
 
-cp spark*/conf/spark-env.sh.template spark*/conf/spark-env.sh
-echo "export MESOS_NATIVE_LIBRARY=/usr/local/lib/libmesos.so" >> spark*/conf/spark-env.sh
-echo "export SPARK_EXECUTOR_URI=http://d3kbcqa49mib13.cloudfront.net/spark-1.0.0-bin-hadoop2.tgz" >> spark*/conf/spark-env.sh
-echo "-Djava.net.preferIPv4Stack=true" >> spark*/conf/java-opts
+wget "http://d3kbcqa49mib13.cloudfront.net/${SPARK_VERSION}.tgz"
+tar zxvf "${SPARK_VERSION}.tgz"
+
+cp "${SPARK_VERSION}/conf/spark-env.sh.template" "${SPARK_VERSION}/conf/spark-env.sh"
+echo "export MESOS_NATIVE_LIBRARY=/usr/local/lib/libmesos.so" >> "${SPARK_VERSION}/conf/spark-env.sh"
+echo "export SPARK_EXECUTOR_URI=http://d3kbcqa49mib13.cloudfront.net/spark-1.0.0-bin-hadoop2.tgz" >> "${SPARK_VERSION}/conf/spark-env.sh"
+echo "-Djava.net.preferIPv4Stack=true" >> "${SPARK_VERSION}/conf/java-opts"
 
 git clone https://github.com/pkoperek/nibbler.git
