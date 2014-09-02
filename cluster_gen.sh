95cf34c4f1ee953df717623216343d09145b25d3
Pawel Koperek <pkoperek@gmail.com>
2014-09-02 20:15:42 +0100

    make /etc/hosts consistent across all nodes

--- cluster_gen.sh
+++ cluster_gen.sh
@@ -127,13 +127,13 @@ for I in `seq $SLAVES_COUNT`; do
 	echo "SLAVE_NO=$I" >> "$TMP_SETUP_FILE"
 	cat "slave_setup.sh" >> "$TMP_SETUP_FILE"
 
-    echo "${SLAVE_IP}       slave${I}" >> "${HOSTS_TMP}"
+	echo "${SLAVE_IP}       slave${I}" >> "${HOSTS_TMP}"
 
 	echo -n "slave $I (${SLAVE_IP}): " >> "$CLUSTER_ACCESS"
 	setupVM "$SLAVE_IP" "$TMP_SETUP_FILE" "$TMP_SETUP_FILE" "$CLUSTER_ACCESS"
 
 	forwardPort "$SLAVE_IP" "22" "SLAVE_OUT_IP" "SLAVE_OUT_PORT"
-    HOSTS_PROPAGATION_LIST="${HOSTS_PROPAGATION_LIST};${SLAVE_OUT_IP}:${SLAVE_OUT_PORT}"
+	HOSTS_PROPAGATION_LIST="${HOSTS_PROPAGATION_LIST};${SLAVE_OUT_IP}:${SLAVE_OUT_PORT}"
 
 	echo "Done."
 done;
